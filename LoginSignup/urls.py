3edf1f64a5af87b544abbcceae22d31a5ceef50c
Sameeran Bandishti <sameeranbandishti93@gmail.com>
2018-12-12 17:27:45 +0530

    Set Up views.py

--- LoginSignup/urls.py
+++ LoginSignup/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='LoginSignup'
 
 urlpatterns=[
-    url(r'^Signup/$',views.Signup,name='Signup')
+    url(r'^Signup/$',views.Signup,name='Signup'),
+    url(r'Login/$',views.Login,name='Login'),
 ]
--- LoginSignup/views.py
+++ LoginSignup/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from .forms import UserForm
+from .forms import UserProfileForm
 # Create your views here.
+
+
+def IndexView(request):
+    return render(request,'index.html')
+
+
+def SignupView(request):
+
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        user_profile_form = UserProfileForm(data=request.POST)
+
+        # Check if the forms are valid
+        if user_form.is_valid() and user_profile_form.is_valid():
+
+            # Hashing the password
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            # Making the one to one relation
+            profile = user_profile_form.save(commit=False)
+            profile.user = user
+            # Checking for Profile Pictures
+            if 'ProfilePicture' in request.FILES:
+                profile.ProfilePicture = request.FILES['ProfilePicture']
