06aebc5d04b19245a4d1363784b793c1706ddaec
SameeranB <sameeranbandishti93@gmail.com>
2019-02-15 22:47:10 +0530

    pls work

--- Enigma/settings.py
+++ Enigma/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_USE_TLS = True
 ALLOWED_HOSTS = ['https://enigma5.herokuapp.com/']
