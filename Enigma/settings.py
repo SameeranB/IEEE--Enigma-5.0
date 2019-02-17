2a79a16e3d432616ae627af5d49ca2859b440bb6
SameeranB <sameeranbandishti93@gmail.com>
2019-02-17 23:40:03 +0530

    Adding _meta line to settings

--- Enigma/settings.py
+++ Enigma/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+from users.models import CustomUser
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -151,6 +152,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# email unique
+
+CustomUser._meta.local_fields[4].__dict__['_unique'] = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
