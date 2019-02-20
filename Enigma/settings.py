c2b0f6e93f416a110e0f847150869101ba38012d
SameeranB <sameeranbandishti93@gmail.com>
2019-02-20 08:55:58 +0530

    Removed Unnecessary Validators

--- Enigma/settings.py
+++ Enigma/settings.py
@@ -140,16 +140,6 @@ PASSWORD_HASHERS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS':{'min_length':8}
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
