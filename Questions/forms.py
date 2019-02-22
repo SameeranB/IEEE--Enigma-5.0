0e4da4e0409082aebe736f62d21776dbb8781f3b
Sameeran Bandishti <sameeranbandishti93@gmail.com>
2019-02-22 13:22:49 +0530

    Auto stash before merge of "Test" and "origin/Test"

--- Questions/forms.py
+++ Questions/forms.py
@@ -10,4 +10,8 @@ class AnswerForm(forms.ModelForm):
 
 
     Answer = forms.CharField(widget=forms.Textarea, label='')
-    HintUsed = forms.CharField(widget=forms.HiddenInput(attrs={'id': 'check'}), label='')
+    HintUsed = forms.CharField(widget=forms.HiddenInput(attrs={'id': 'check'}), label='', required=False)
+
+    def clean(self):
+        ans = self.cleaned_data['Answer']
+        print(ans)
