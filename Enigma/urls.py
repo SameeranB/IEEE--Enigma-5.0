c7172e1ea0e4de84b5305c5e10318a46e4a8ee4c
SameeranB <sameeranbandishti93@gmail.com>
2019-02-22 20:35:46 +0530

    Static

--- Enigma/urls.py
+++ Enigma/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     # url(r'^reset-password/reset-password-done/$', PasswordResetDoneView.as_view(),{'template_name': 'LoginSignup/reset/reset_password_done.html'}, name='password_reset_done'),
     # url(r'^reset-password/password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(),{'template_name': 'LoginSignup/reset/reset_password_confirm.html', 'post_reset_redirect': 'password_reset_complete'}, name='password_reset_confirm'),
     # url(r'^reset-password/reset-password-complete/$', PasswordResetCompleteView.as_view(),{'template_name': 'LoginSignup/reset/reset_password_complete.html'}, name='password_reset_complete'),
-    url(r'^.*$', views.InfLoad, name='Inf'),
+    url(r'^.*$', views.InfLoad.as_view(), name='Inf'),
 ]
